098da42d4c6e07ff1a9e23380358c803a8c7160e
spanme <asitsn9ine@gmail.com>
2025-02-04 12:20:51 +0000

    updated to babybot specs

---
+++ index.ts
@@ -0,0 +1,29 @@
+import DiscordJS, {GatewayIntentBits} from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.on('ready', () => {
+    console.log('Ready!')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong'
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
+
+
+
+
